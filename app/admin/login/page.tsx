667c5480f2b179247097f4f62a0f2a2ad88ed040
honghao30 <58649794+honghao30@users.noreply.github.com>
2025-03-19 11:07:38 +0900

    로그인 페이지 구성중

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { Button, Stack } from '@mui/material';
 
 export default function LoginPage(){
     return (
@@ -20,8 +21,12 @@ export default function LoginPage(){
                 maxWidth: '300px',
                 gap: '10px',
             }}>
-                <TextField fullWidth label="User ID" id="fullWidth" />
+                <TextField fullWidth label="ID" id="fullWidth" />
                 <TextField fullWidth label="PassWord" id="fullWidth" />
+                <Stack spacing={2} direction="row">
+                    <Button variant="contained">로그인</Button>
+                    <Button variant="outlined">가입</Button>
+                </Stack>
             </Box>
             
         </Box>
